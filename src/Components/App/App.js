9a5a6b81393478902f7e40f5a66db89d1539d9a8
Jade Shi <ziyushi1997@gmail.com>
2023-09-13 09:34:13 -0700

    install uuid

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from '../Header/Header';
 import ArticlesContainer from '../ArticlesContainer/ArticlesContainer';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ArticlesContainer />
+      <Routes>
+      <Route path='/' element={<ArticlesContainer />}/>
+      </Routes>
     </div>
   );
 }
