e7dc1575ea9453792496a3a5ce282d6ce0bc1334
Jade Shi <ziyushi1997@gmail.com>
2023-09-14 08:11:07 -0700

    Add hover effects to button

--- src/Components/ArticleView/ArticleView.js
+++ src/Components/ArticleView/ArticleView.js
@@ -1,21 +1,33 @@
-import './ArticleView.css';
-import { useParams } from 'react-router-dom';
+import "./ArticleView.css";
+import { useParams } from "react-router-dom";
 
-export default function ArticleView({articles}) {
+export default function ArticleView({ articles }) {
   const { id } = useParams();
-  const article = articles.filter(article => article.id.includes(id))[0];
-  const {source, author, title, description, url, urlToImage, formatDate, content} = article;
+  const article = articles.filter((article) => article.id.includes(id))[0];
+  const {
+    source,
+    author,
+    title,
+    description,
+    url,
+    urlToImage,
+    formatDate,
+    content,
+  } = article;
   return (
-    <div className='article-view'>
-      <h1 className='title'>{title}</h1>
-      <img src={urlToImage} alt='Cover'/>
-      <a href={url}>source</a>
-      <p>{author}</p>
-      <p>{formatDate}</p>
-      <p>{source.name}</p>
-      <p className='description'>{description}</p>
-      <p className='content'>{content}</p>
+    <div className="article-view">
+      <h1 className="title">{title}</h1>
+      <div className="writer-info">
+        <p>By {author}</p>
+        <p>•</p>
+        <p>Published at {formatDate}</p>
+      </div>
+      <img src={urlToImage} alt="Cover" />
+      <p className="description">{description}</p>
+      <p className="content">{content}</p>
+      <button className="source-button">
+        <a href={url}>read the full article at {source.name}</a>
+      </button>
     </div>
-
-  )
+  );
 }
