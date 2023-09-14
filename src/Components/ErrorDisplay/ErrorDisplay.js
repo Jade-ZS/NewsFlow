5d2d602fbc345a16ec28ce852b870a79aec98b42
Jade Shi <ziyushi1997@gmail.com>
2023-09-14 10:24:07 -0700

    Style error display page

--- src/Components/ErrorDisplay/ErrorDisplay.js
+++ src/Components/ErrorDisplay/ErrorDisplay.js
@@ -1,10 +1,12 @@
-import './ErrorDisplay';
+import './ErrorDisplay.css';
+import { Navigate } from 'react-router-dom';
 
 export default function ErrorDisplay() {
   return (
     <div className='error-display'>
-      <h1>err</h1>
+      <h1>Ooopsy...Page Not Found</h1>
       <img src='https://i.gifer.com/jU.gif' alt='crying face'/>
+      <p>We are sorry, it seems that something went wrong</p>
     </div>
   );
 }
