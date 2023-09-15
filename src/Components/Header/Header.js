af715cf382ca2822e3be42fef3e004ef31888c5a
Jade Shi <ziyushi1997@gmail.com>
2023-09-14 18:02:03 -0700

    Add drop down

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,68 @@
 import "./Header.css";
-import { Link, useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
-  const [searchInput, setSearchInput] = useState('');
+  const {innerWidth: width, innerHeight: height} = window;
+  const [searchInput, setSearchInput] = useState("");
   const [, setSearchParam] = useSearchParams();
-  
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const [isPortrait, setIsPortrait] = useState(false);
+
+  useEffect(() => {
+    if (width < height) {
+      setIsPortrait(false)
+    } else {
+      setIsPortrait(true)
+    }
+  }, [width, height])
+
   const handleSearch = () => {
     if (!searchInput.length) {
-      alert('This field cannot be empty');
+      alert("This field cannot be empty");
       return;
     }
-    setSearchParam({'search': searchInput});
-    setSearchInput('');
-  }
+    setSearchParam({ search: searchInput });
+    setSearchInput("");
+  };
+
+  const handleMenu = () => {
+    setShowSearchBar(prev => !prev);
+  };
+
+  const searchBar = (
+    <div className="search-bar">
+      <input
+        className="search-input"
+        placeholder="Search by headline"
+        onChange={(e) => setSearchInput(e.target.value)}
+        value={searchInput}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        SEARCH
+      </button>
+    </div>
+  );
+
+  const dropDown = (
+    <div className='drop-down'>
+      {searchBar}
+    </div>
+  );
 
   return (
     <header>
-      <Link to='/'>
-        <h1 className='app-title'>NewsFlow</h1>
+      <Link to="/">
+        <h1 className="app-title">NewsFlow</h1>
       </Link>
-      <img className='menu-icon'  src={require('../../Assets/menu.png')}/>
-      <div className="search-bar">
-        <input
-          className="search-input"
-          placeholder="Search by headline"
-          onChange={e => setSearchInput(e.target.value)}
-          value={searchInput}
-        />
-        <button className="search-button" onClick={handleSearch}>SEARCH</button>
-      </div>
+      <img 
+        className='menu-icon'
+        src={require("../../Assets/menu.png")} 
+        alt='menu'
+        onClick={handleMenu}
+      />
+      {searchBar}
+      {dropDown}
     </header>
   );
 }
